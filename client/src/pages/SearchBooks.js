d4e63708c17fd6c1e55f993143b19d1174a365c3
Chewie <chewie1985@gmail.com>
2021-08-08 21:09:36 -0600

    still importing and installing dependencies

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -6,6 +6,7 @@ import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/react-hooks';
 import { SAVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
